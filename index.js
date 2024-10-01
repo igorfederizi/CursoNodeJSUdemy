1c45ee801bca0886896928460cf154b62e5cc5db
Igor Federizi <igor.federizi@gmail.com>
2024-10-01 08:59:01 -0300

    Creating my first route, get

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const express = require("express");
 // função que carrega o express
 const app = express();
 
+
+//criando rota
+app.get("/", function(req, res) {
+res.send("Bem vindo à rota!")
+});
+
+app.get("/blog", function(req, res) {
+res.send("bem vindo ao blog")
+});
+
+app.get("/canal/youtube", function(req, res) {
+    res.send("Bem vindo ao youtube");
+})
+
+
 // iniciando o servidor
 app.listen(4000, function(error) {
     if(error){
